c23906e077a99bc50e12a550c530fa71c2d97330
Wesley TAHIRI <tahiriwesleyalexandre@gmail.com>
2026-01-28 12:53:23 +0100

    Ajout de la classe DiscountService pour le calcul des remises en fonction du type de client.

---
+++ DiscountService.cs
@@ -0,0 +1,26 @@
+public class DiscountService
+{
+    public double Calc(double amount, string type)
+    {
+        if (type == "VIP")
+        {
+            if (amount > 1000)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.1;
+            }
+        }
+        else
+        {
+            if (amount > 500)
+            {
+                return amount * 0.05;
+            }
+        }
+
+        return 0;
+    }
+}
