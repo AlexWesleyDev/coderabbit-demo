9efc00172eed04dd07ab63c8cf9c704eaf982763
Wesley TAHIRI <tahiriwesleyalexandre@gmail.com>
2026-01-28 13:56:58 +0100

    Fichier 2

---
+++ DiscountService1.cs
@@ -0,0 +1,29 @@
+public class DiscountService
+{
+    public double Calc(double amount, string type)
+    {
+        if (type == "VIP")
+        {
+            if (amount > 1000)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.1;
+            }
+        }
+        else
+        {
+            if (type == "Regular" || type == "Premium")
+            {
+                if (amount > 500)
+                {
+                    return amount * 0.05;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
